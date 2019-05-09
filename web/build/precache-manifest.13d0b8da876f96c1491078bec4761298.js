1f52edae2e7fcdda7fd23912a251289f38c81893
Patrick Hooper <patrick.hooper.m@outlook.com>
2019-05-08 20:16:54 -0400

    adding productionized files

---
+++ web/build/precache-manifest.13d0b8da876f96c1491078bec4761298.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff6eef00e683178f7ade2baf4b60f5be",
+    "url": "/static/media/logo.ff6eef00.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "309da800c6ff55393f74",
+    "url": "/static/js/main.309da800.chunk.js"
+  },
+  {
+    "revision": "f2c6011bab76b5a7c03c",
+    "url": "/static/js/1.f2c6011b.chunk.js"
+  },
+  {
+    "revision": "309da800c6ff55393f74",
+    "url": "/static/css/main.0b124fa5.chunk.css"
+  },
+  {
+    "revision": "f2c6011bab76b5a7c03c",
+    "url": "/static/css/1.7087b61c.chunk.css"
+  },
+  {
+    "revision": "255fbd0dd5674556e59cfe3e4e5d3821",
+    "url": "/index.html"
+  }
+];
